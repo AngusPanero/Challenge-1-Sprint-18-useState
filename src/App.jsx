cf49c9b122c40280219a34dab5f538bb303f68c0
Agustin Panero <agustinpanero@MacBook-Pro-de-Agustin.local>
2025-03-05 14:48:11 -0300

    TERMINADO

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
 import './App.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { useState } from "react"
 
 function App() {
- //Aquí tu código
+    const [ name, setName ] = useState("Sofia")
+
+    return(
+        <div>
+            <h1>Teacher Name: {name}</h1>
+            <ul>
+                <button onClick={() => setName("Data")}><li>Data</li></button>
+                <button onClick={() => setName("Reyes")}><li>Reyes</li></button>
+                <button onClick={() => setName("Yolanda")}><li>Yolanda</li></button>
+            </ul>
+        </div>
+    )
 }
 
-export default App
+function ChangeName() {
+    const [ name, setName ] = useState("Sofia")
+    const [ newName, setNewName ] = useState("") 
+
+    const CambiarNombre = (e) => {
+        e.preventDefault()
+        setName(newName)
+    }
+
+    return(
+        <div>
+            <h1>Teacher Name: {name}</h1>
+            <form onSubmit={CambiarNombre}>
+                <input type="text" value={newName} onChange={(evento) => setNewName(evento.target.value)} placeholder='Add a Name'/>
+                <button type="submit">Add Name</button>
+            </form>
+        </div>
+    )
+}
+
+export { App, ChangeName }
